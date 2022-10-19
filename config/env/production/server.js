7e8f8b6ebb7decee4d41a2fabaa61a42300eb52c
thomas <hello@thomasflorentin.net>
2022-10-19 14:20:48 +0200

    config

---
+++ config/env/production/server.js
@@ -0,0 +1,8 @@
+module.exports = ({ env }) => ({
+    host: env('pactice-strapi-cms.herokuapp.com', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    proxy: true,
+    app: {
+      keys: env.array('APP_KEYS', ["testKey1", "testKey2"]),
+    },
+  });
--- config/server.js
+++ config/server.js
@@ -1,8 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('pactice-strapi-cms.herokuapp.com', '0.0.0.0'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  proxy: true,
   app: {
-    keys: env.array('APP_KEYS', ["testKey1", "testKey2"]),
+    keys: env.array('APP_KEYS'),
   },
 });
