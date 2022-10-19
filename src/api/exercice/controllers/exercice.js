7b29efe010b5112d4132389c0cf56cb17c7b6ccf
thomas <hello@thomasflorentin.net>
2022-10-19 14:44:29 +0200

    contentType: Exercices

---
+++ src/api/exercice/controllers/exercice.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * exercice controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::exercice.exercice');
---
+++ src/api/exercice/routes/exercice.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * exercice router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::exercice.exercice');
